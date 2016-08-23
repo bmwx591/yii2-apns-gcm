39f42b8be4e88bb43dcf8415888c68bf1e49a365
ivan <bmw_x591@mail.ru>
2016-08-23 11:38:28 +0300

    Fix bug by send message

--- Gcm.php
+++ Gcm.php
@@ -75,10 +75,7 @@ class Gcm extends AbstractApnsGcm
         try {
             // send a message
             $result = $this->getClient()->send($message, $token, $this->retryTimes);
-            $this->success = $result->getErrorCode() != null ? false : true;
-            if (!$this->success) {
-                $this->errors[] = $result->getErrorCode();
-            }
+            $this->success = $result->getSuccess();
             // HTTP code 200, but message sent with error
         } catch (\InvalidArgumentException $e) {
             $this->errors[] = $e->getMessage();
@@ -98,79 +95,8 @@ class Gcm extends AbstractApnsGcm
         return $message;
     }
 
-    /**
-     * send a push notification for android using GCM client
-     *
-     * Usage 1:
-     * <code>
-     * $this->sendMulti(
-     *  'some-valid-token',
-     *  'some-message',
-     *  [
-     *   'custom_data_key_1' => 'custom_data_value_1',
-     *   'custom_data_key_2' => 'custom_data_value_2',
-     *  ]
-     * );
-     * </code>
-     *
-     * Usage 2:
-     * <code>
-     * $this->sendMulti(
-     *  ['valid-token-1','valid-token-2','valid-token-3'],
-     *  'some-message',
-     *  [
-     *   'custom_data_key_1'=>'custom_data_value_1',
-     *   'custom_data_key_2'=>'custom_data_value_2',
-     *  ]
-     * );
-     * </code>
-     * @param string|array $tokens
-     * @param $text
-     * @param array $payloadData
-     * @param array $args
-     * @return null|\PHP_GCM\Message
-     */
     public function sendMulti($tokens, $text, $payloadData = [], $args = [])
     {
-        $tokens = is_array($tokens) ? $tokens : [$tokens];
-        // check if its dry run or not
-        if ($this->dryRun === true) {
-            $this->log($tokens, $text, $payloadData, $args);
-            $this->success = true;
-            return null;
-        }
-
-        $message = new \PHP_GCM\Message();
-        foreach ($args as $method => $value) {
-            $value = is_array($value) ? $value : [$value];
-            call_user_func_array([$message, $method], $value);
-        }
-        // set a custom payload data
-        $payloadData['message'] = $text;
-        foreach ($payloadData as $key => $value) {
-            $message->addData($key, $value);
-        }
-        try {
-            // send a message
-            $result = $this->getClient()->sendMulti($message, $tokens, $this->retryTimes);
-
-            $this->success = $result->getSuccess();
-        } catch (\InvalidArgumentException $e) {
-            $this->errors[] = $e->getMessage();
-            // $deviceRegistrationId was null
-        } catch (\PHP_GCM\InvalidRequestException $e) {
-            if ($e->getMessage()) {
-                $this->errors[] = $e->getMessage();
-            } else {
-                $this->errors[] = sprintf("Received error code %s from GCM Service", $e->getCode());
-            }
-            // server returned HTTP code other than 200 or 503
-        } catch (\Exception $e) {
-            $this->errors[] = $e->getMessage();
-            // message could not be sent
-        }
-
-        return $message;
     }
 
     public function __call($method, $params)
